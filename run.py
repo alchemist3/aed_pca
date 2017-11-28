9a2c768eed99313a18de971bb984b80a5eb3fea7
rafalk <alchemist3@wp.pl>
2017-11-28 20:31:06 +0100

    Show sorted eigen values

--- run.py
+++ run.py
@@ -10,20 +10,28 @@ if sys.version_info < (3, 6):
 iris_pca = Pca()
 iris_pca.load_arff('iris.arff')
 iris_pca.pca()
+print(50*"#")
 print(
-    "Zbiór iris - składowe główne wyjaśniające 90% zmiennych oryginalnych [stosunek wyjaśniania: numer składowej głównej]:")
+    "Zbiór iris - składowe główne wyjaśniające 90% zmiennych oryginalnych [procent wyjaśnienia: numer składowej głównej]:")
 print(iris_pca._90p_eigen_values)
+print("Posortowany rosnąco wektor wartości własnych [wartość własna: nr wartości własnej]")
+pprint.pprint(iris_pca.sorted_eigen_values)
+print(50*"#")
 iris_pca.print2d_pca()
 iris_pca.print2d_rand()
 iris_pca.print3d_pca()
 iris_pca.print3d_rand()
 
+
 wave_pca = Pca()
 wave_pca.load_arff('waveform5000.arff')
 wave_pca.pca()
 print(
-    "Zbiór waveform5000 - składowe główne wyjaśniające 90% zmiennych oryginalnych [stosunek wyjaśniania: numer składowej głównej]:")
+    "Zbiór waveform5000 - składowe główne wyjaśniające 90% zmiennych oryginalnych [procent wyjaśnienia: numer składowej głównej]:")
 pprint.pprint(wave_pca._90p_eigen_values)
+print("Posortowany rosnąco wektor wartości własnych [wartość własna: nr wartości własnej]")
+pprint.pprint(wave_pca.sorted_eigen_values)
+print(50*"#")
 
 # To increase plot generation speed, only 10% of data is plotted
 # If no argument is given all data will be plotted.
